a62b3a18950721924cdded5059bfa1e6d967fca9
Helene <blandin.helene@gmail.com>
2022-03-12 13:03:09 +0100

    Base Landing-Page

--- src/Controller/LandingPageController.php
+++ src/Controller/LandingPageController.php
@@ -2,19 +2,38 @@
 
 namespace App\Controller;
 
+use App\Entity\Competence;
+use App\Entity\Experience;
+use App\Entity\Formation;
+use App\Entity\Leisure;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LandingPageController extends AbstractController
 {
+    private EntityManagerInterface $em;
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     /**
      * @Route("/landing/page", name="landing_page")
      */
     public function index(): Response
-    {
+    {   
+        $competences = $this->em->getRepository(Competence::class)->findAll();
+        $formations = $this->em->getRepository(Formation::class)->findAll();
+        $experiences = $this->em->getRepository(Experience::class)->findAll();
+        $loisirs = $this->em->getRepository(Leisure::class)->findAll();
+
         return $this->render('landing_page/index.html.twig', [
-            'controller_name' => 'LandingPageController',
+            'competences' => $competences,
+            'formations' => $formations,
+            'experiences' => $experiences,
+            'loisirs' => $loisirs,
         ]);
     }
 }
